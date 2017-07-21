102570ea94caf9b0cb6da3c198ed69967941b3e8
ferhoba <ferhoba@gmail.com>
2017-07-21 14:43:08 +0200

    this is a last commit

--- js/jQuery.linkIt.js
+++ js/jQuery.linkIt.js
@@ -3,7 +3,25 @@
  * Version : 0.1.0.0
  */
 (function ($) {
-    $.fn.linkIt = function () {
-        alert(this.html());
+    $.fn.linkIt = function (options) {
+        var settings = $.extend({
+            href    :null,
+            text    :null,
+            target  :"_self"
+        }, options);
+        // Validation
+        if(settings.href === null){
+            console.log("You need an href option applied to LinkIt work fine");
+            return this;
+        }
+        return this.each(function () {
+            var object = $(this); //all object where LinkIt is applied
+
+            // If new text is NOT added plugin will just use an original text from document
+            if(settings.text === null){
+                settings.text = object.text();
+            }
+            object.wrap('<a target="'+settings.target+'" href = "'+settings.href+'"></a>').text(settings.text);
+        });
     }
 }(jQuery));
